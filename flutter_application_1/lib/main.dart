89e5e99d294fd9eb7e5c64e15f93a95500060dd7
StevenLim <stevenlim_2226240159@mhs.mdp.ac.id>
2026-04-29 20:09:01 +0700

    progres

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/screens/sign_in_screen.dart';
 import 'package:flutter_application_1/screens/sign_up_screen.dart';
 
 Future<void> main() async {
@@ -29,7 +30,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: const SignUpScreen(),
+      home: const SignInScreen(),
     );
   }
 }
